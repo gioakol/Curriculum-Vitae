6acc1add1d498e99919a1d9d53e9d64d05dcd065
Giovanni Beltran <gioakol@gmail.com>
2024-05-19 15:55:33 -0500

    mod: work experience
    mod: work experience section
    add: link curriculum vitae
    
    Solve issue #4

--- assets/js/main.js
+++ assets/js/main.js
@@ -235,23 +235,23 @@
     }
   });
 
- /**
-   * portfolio slider
-   */
- new Swiper('.portfolio-slider', {
-  speed: 600,
-  loop: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false
-  },
-  slidesPerView: 'auto',
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true
-  }
-});
+  /**
+    * portfolio slider
+    */
+  new Swiper('.portfolio-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
 
   /**
    * applications slider
